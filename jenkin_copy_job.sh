17ad2df1c7aa44edd0163afb216fdc3d11d55b62
Hamzah Jamad <hamzahjamad@gmail.com>
2016-05-30 12:01:52 +0800

    fixing the script

--- jenkin_copy_job.sh
+++ jenkin_copy_job.sh
@@ -3,18 +3,24 @@
 runcurl(){
 	jkusername=$1 
 	jkapi=$2 
-	$newjobname=${3}
-	$jobtocopy=${4}
+	newjobname=$3
+	jobtocopy=$4
 	jkurl=$5 
-  	echo "curl --user $jkusername:$jkapi --data \"name$newjobname&modecopy&from=$jobtocopy\" http://$jkurl/createItem"
+
+	#this will copy config of existing job
+	curl --user $jkusername:$jkapi "http://$jkurl/job/$jobtocopy/config.xml" > config.xml
+
+
+	#write a script to edit the config git - TODO
+
+	#this will create a job and use the copied config
+	curl --user $jkusername:$jkapi -X POST -H "Content-Type:application/xml" -d @config.xml "http://$jkurl/createItem?name=$newjobname"
 }
-
-
 
 #
 # Check input params
 #
-if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" ]; then 
+if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "$4" -a -n "$5" ]; then 
     runcurl $1 $2 $3 $4 $5
 else
     echo "Usage:"
